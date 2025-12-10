49f2d67a76dce0ee4bd3e279d2c6116b90f6117c
Mayara Almeida <mayara.s.almeida8@aluno.senai.br>
2025-12-10 08:32:55 -0300

    Ajustes finais tela cadastro

--- obj/Debug/net9.0/ProjetoCinemanticaMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjetoCinemanticaMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d9110bbbe360e86b559e37da24727be80bc5db4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0bd87f03a7154acd7938acd3380fd67ab4ae748")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
