e37493168c0617f7faed58f94ed4ed8c7dff3e19
GChicarelli <giulianochicarelli@gmail.com>
2025-12-12 08:38:02 -0300

    Correção de bugs

--- obj/Debug/net9.0/ProjetoCinemanticaMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjetoCinemanticaMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dddc85085dc39a49c792b4ccbc36995ff9578330")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a067d7d72df57f5db724561fa9b9276c59c368dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
