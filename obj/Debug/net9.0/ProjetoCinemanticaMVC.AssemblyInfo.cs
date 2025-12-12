472bfdbac876b637b45b7416d105cb8e8ada8ef6
Raphael-Aljona <raphael.aljona@gmail.com>
2025-12-12 10:46:00 -0300

    Ver perfil adicionado

--- obj/Debug/net9.0/ProjetoCinemanticaMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjetoCinemanticaMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e970389e3000e75d496ad6f7e330584aedc1884a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2dd4946cffca85631c2ff2d31cc27c979f55cb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
