e4eae16c41a3bad4ea5fbd7c31c2e3ea8b4c02a2
Raphael-Aljona <raphael.aljona@gmail.com>
2025-12-15 10:59:17 -0300

    Avaliações

--- Controllers/AvaliacoesController.cs
+++ Controllers/AvaliacoesController.cs
@@ -8,15 +8,6 @@ namespace ProjetoCinemanticaMVC.Controllers // Verifique se o namespace é o mes
 {
     public class AvaliacoesController : Controller
     {
-        // GET: Avaliacoes/Feed
-        // Carrega o arquivo: Views/Avaliacoes/feed_avaliacoes.cshtml
-        // public ActionResult Feed()
-        // {
-        //     return View("feed_avaliacoes");
-        // }
-
-        // GET: Avaliacoes/Nova
-        // Carrega o arquivo: Views/Avaliacoes/nova_avaliacao.cshtml
         public ActionResult Nova(string title, string poster)
         {
             // Passamos os parâmetros via ViewBag caso queira usar C# no futuro, 
@@ -51,21 +42,21 @@ namespace ProjetoCinemanticaMVC.Controllers // Verifique se o namespace é o mes
             Console.WriteLine("MovieId = " + movieId);
 
             var comentarios = _appDbContext.Comentarios
-      .Include(c => c.id_usuarioNavigation)
-      .OrderByDescending(c => c.data_post)
-      .Select(c => new AvaliacoesViewModel
-      {
-          nome_usuario = c.id_usuarioNavigation.nick_name,
-          TituloFilme = c.nome_filme,
-        PosterFilme = c.img_path,   
-          descricao = c.descricao,
-          data_post = c.data_post,
-          usuario = c.id_usuarioNavigation,
-          foto_perfil = c.id_usuarioNavigation.foto_perfil != null
-              ? Convert.ToBase64String(c.id_usuarioNavigation.foto_perfil)
-              : null
-      })
-      .ToList();
+            .Include(c => c.id_usuarioNavigation)
+            .OrderByDescending(c => c.data_post)
+            .Select(c => new AvaliacoesViewModel
+            {
+                    nome_usuario = c.id_usuarioNavigation.nick_name,
+                    TituloFilme = c.nome_filme,
+                    PosterFilme = c.img_path,   
+                    descricao = c.descricao,
+                    data_post = c.data_post,
+                    usuario = c.id_usuarioNavigation,
+                    foto_perfil = c.id_usuarioNavigation.foto_perfil != null
+                    ? Convert.ToBase64String(c.id_usuarioNavigation.foto_perfil)
+                    : null
+            })
+            .ToList();
 
             return View("~/Views/Avaliacoes/feed_avaliacoes.cshtml", comentarios);
 
