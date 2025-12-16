3a2c65e220eb87d0d96cfed4b0384938d4dcad53
Mayara Almeida <mayara.s.almeida8@aluno.senai.br>
2025-12-16 01:06:13 -0300

    Ajuste na tela login

--- obj/Debug/net9.0/ProjetoCinemanticaMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjetoCinemanticaMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ab5f9e0c06fbd1145f2310401727b8f4a96f92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+336dd8e55d64a6355ccebb8e27aa29cb86678425")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoCinemanticaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
